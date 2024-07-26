765b1020163a935eafc3aa36997ad4ff76f21a5a
RakibulHasan95 <rakibul.hasan.967539@gmail.com>
2024-07-26 08:06:35 +0600

    front page is modified

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Web-Developer", ""],
+        strings: ["", "Front-End Developer", ""],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -71,3 +71,4 @@ $(document).ready(function(){
         }
     });
 });
+
